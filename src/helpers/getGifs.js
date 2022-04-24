fa54cd26cfab64de98a4b8cfb06fd5eb68e1f32d
Mauro Vargas <mauro.vargas@catodigo.co>
2022-04-24 05:28:14 -0500

    Fix https protocol on fetch api

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=JQAryVjVsSi2I9AyguXwKLZ8vHT52lMu`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=JQAryVjVsSi2I9AyguXwKLZ8vHT52lMu`;
 
     const resp = await fetch(url);
     const { data } = await resp.json();
